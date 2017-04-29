33d3db5569cf50302bd143ea13374e0c4f116ec5
harspeck <has245@cornell.edu>
2017-04-28 21:16:47 -0400

    Add files via upload

--- FinalProject/js/scripts.js
+++ FinalProject/js/scripts.js
@@ -24,3 +24,44 @@ $(function(){
     e.preventDefault(); // stop form submission
   });
 });
+
+
+$(document).ready(function () {
+    $('#maptoggle').on('click', function(e){
+      e.preventDefault();
+      // create menu variables
+      var screenWidth = $(window).width();
+      var slideoutMap = $('#mapcontainer');
+      var slideoutMapWidth = $('#mapcontainer').width();
+      var slideoutCom = $('#leaveComment');
+      // toggle open class
+      slideoutMap.toggleClass("open");
+      slideoutCom.toggleClass("open");
+
+      if (slideoutCom.hasClass("open")) {
+        slideoutCom.animate({
+          left: 0
+        }); 
+      } else {
+        slideoutCom.animate({
+          left: -slideoutMapWidth*.4
+        }, 250)
+      }
+      // slide menu
+      if (slideoutMap.hasClass("open")) {
+        $('#maptoggle').css("background-color","black")
+        $('#maptoggle').css("opacity",".7")
+        slideoutMap.animate({
+          right: 0
+        }); 
+      } else {
+        $('#maptoggle').css("background-color","#b00d10")
+        $('#maptoggle').css("opacity","1")
+        slideoutMap.animate({
+          right: -slideoutMapWidth*2
+        }, 250)
+      }
+    });
+    $(window).resize(function(){
+    });
+});
